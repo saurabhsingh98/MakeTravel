dbb87b0b29707245cc2b0b8f94ecaf70f813931c
Saurabh Singh <dashingsaurabh1996@gmail.com>
2024-09-20 00:19:18 +0530

    link updata

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'https://travel-booking-tvgv.onrender.com/api/v1'
-export const BASE_URL= 'http://localhost:8000/api/v1'
+export const BASE_URL= 'https://travelbackend-u3yi.onrender.com/api/v1'
